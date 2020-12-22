451755f041ec17968a586c4ee1441a6567964f72
23456-Tony-Larrea <52183482+23456-Tony-Larrea@users.noreply.github.com>
2020-12-21 22:49:33 -0500

    chritmas

---
+++ CandysChristmas/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace CandysChristmas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.over = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.baston1 = new System.Windows.Forms.PictureBox();
+            this.baston2 = new System.Windows.Forms.PictureBox();
+            this.caramelo1 = new System.Windows.Forms.PictureBox();
+            this.caramelo2 = new System.Windows.Forms.PictureBox();
+            this.baston3 = new System.Windows.Forms.PictureBox();
+            this.caramelo3 = new System.Windows.Forms.PictureBox();
+            this.enemigo2 = new System.Windows.Forms.PictureBox();
+            this.enemigo3 = new System.Windows.Forms.PictureBox();
+            this.enemigo = new System.Windows.Forms.PictureBox();
+            this.noel = new System.Windows.Forms.PictureBox();
+            this.obstaculo6 = new System.Windows.Forms.PictureBox();
+            this.obstaculo5 = new System.Windows.Forms.PictureBox();
+            this.obstaculo4 = new System.Windows.Forms.PictureBox();
+            this.obstaculo3 = new System.Windows.Forms.PictureBox();
+            this.obstaculo2 = new System.Windows.Forms.PictureBox();
+            this.obstaculo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.baston1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baston2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caramelo1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caramelo2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baston3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caramelo3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemigo2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemigo3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemigo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.noel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstaculo6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstaculo5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstaculo4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstaculo3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstaculo2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstaculo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // over
+            // 
+            this.over.AutoSize = true;
+            this.over.BackColor = System.Drawing.Color.Yellow;
+            this.over.Font = new System.Drawing.Font("Comic Sans MS", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.over.ForeColor = System.Drawing.Color.Red;
+            this.over.Location = new System.Drawing.Point(285, 253);
+            this.over.Name = "over";
+            this.over.Size = new System.Drawing.Size(227, 48);
+            this.over.TabIndex = 10;
+            this.over.Text = "GAME OVER";
+            this.over.Click += new System.EventHandler(this.over_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Ivory;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 35);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "CARAMELOS=0";
+            // 
+            // baston1
+            // 
+            this.baston1.Image = global::CandysChristmas.Properties.Resources.png_clipart_candy_cane_christmas_gift_scalable_graphics_christmas_cartoon_element_cartoon_character_ribbon;
+            this.baston1.Location = new System.Drawing.Point(608, 253);
+            this.baston1.Name = "baston1";
+            this.baston1.Size = new System.Drawing.Size(54, 31);
+            this.baston1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.baston1.TabIndex = 33;
+            this.baston1.TabStop = false;
+            // 
+            // baston2
+            // 
+            this.baston2.Image = global::CandysChristmas.Properties.Resources.WhatsApp_Image_2020_12_21_at_9_25_03_PM;
+            this.baston2.Location = new System.Drawing.Point(236, 394);
+            this.baston2.Name = "baston2";
+            this.baston2.Size = new System.Drawing.Size(54, 31);
+            this.baston2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.baston2.TabIndex = 31;
+            this.baston2.TabStop = false;
+            // 
+            // caramelo1
+            // 
+            this.caramelo1.Image = global::CandysChristmas.Properties.Resources._515_5156481_dulces_dibujos_png_caramelos_animados_de_colores_png;
+            this.caramelo1.Location = new System.Drawing.Point(709, 96);
+            this.caramelo1.Name = "caramelo1";
+            this.caramelo1.Size = new System.Drawing.Size(35, 26);
+            this.caramelo1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.caramelo1.TabIndex = 17;
+            this.caramelo1.TabStop = false;
+            // 
+            // caramelo2
+            // 
+            this.caramelo2.Image = global::CandysChristmas.Properties.Resources._515_5156481_dulces_dibujos_png_caramelos_animados_de_colores_png;
+            this.caramelo2.Location = new System.Drawing.Point(459, 414);
+            this.caramelo2.Name = "caramelo2";
+            this.caramelo2.Size = new System.Drawing.Size(35, 26);
+            this.caramelo2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.caramelo2.TabIndex = 15;
+            this.caramelo2.TabStop = false;
+            // 
+            // baston3
+            // 
+            this.baston3.Image = global::CandysChristmas.Properties.Resources.png_clipart_candy_cane_christmas_gift_scalable_graphics_christmas_cartoon_element_cartoon_character_ribbon;
+            this.baston3.Location = new System.Drawing.Point(251, 32);
+            this.baston3.Name = "baston3";
+            this.baston3.Size = new System.Drawing.Size(54, 31);
+            this.baston3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.baston3.TabIndex = 12;
+            this.baston3.TabStop = false;
+            // 
+            // caramelo3
+            // 
+            this.caramelo3.Image = global::CandysChristmas.Properties.Resources._515_5156481_dulces_dibujos_png_caramelos_animados_de_colores_png;
+            this.caramelo3.Location = new System.Drawing.Point(45, 266);
+            this.caramelo3.Name = "caramelo3";
+            this.caramelo3.Size = new System.Drawing.Size(35, 26);
+            this.caramelo3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.caramelo3.TabIndex = 11;
+            this.caramelo3.TabStop = false;
+            // 
+            // enemigo2
+            // 
+            this.enemigo2.BackColor = System.Drawing.Color.Transparent;
+            this.enemigo2.Image = global::CandysChristmas.Properties.Resources._557_5575138_grinch_stealing_the_tree_clipart;
+            this.enemigo2.Location = new System.Drawing.Point(153, 234);
+            this.enemigo2.Name = "enemigo2";
+            this.enemigo2.Size = new System.Drawing.Size(81, 67);
+            this.enemigo2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemigo2.TabIndex = 9;
+            this.enemigo2.TabStop = false;
+            // 
+            // enemigo3
+            // 
+            this.enemigo3.BackColor = System.Drawing.Color.Transparent;
+            this.enemigo3.Image = global::CandysChristmas.Properties.Resources._557_5575138_grinch_stealing_the_tree_clipart;
+            this.enemigo3.Location = new System.Drawing.Point(543, 86);
+            this.enemigo3.Name = "enemigo3";
+            this.enemigo3.Size = new System.Drawing.Size(81, 67);
+            this.enemigo3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemigo3.TabIndex = 8;
+            this.enemigo3.TabStop = false;
+            // 
+            // enemigo
+            // 
+            this.enemigo.BackColor = System.Drawing.Color.Transparent;
+            this.enemigo.Image = global::CandysChristmas.Properties.Resources._557_5575138_grinch_stealing_the_tree_clipart;
+            this.enemigo.Location = new System.Drawing.Point(45, 86);
+            this.enemigo.Name = "enemigo";
+            this.enemigo.Size = new System.Drawing.Size(81, 67);
+            this.enemigo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemigo.TabIndex = 7;
+            this.enemigo.TabStop = false;
+            // 
+            // noel
+            // 
+            this.noel.BackColor = System.Drawing.Color.Transparent;
+            this.noel.Image = global::CandysChristmas.Properties.Resources.noel1;
+            this.noel.Location = new System.Drawing.Point(45, 465);
+            this.noel.Name = "noel";
+            this.noel.Size = new System.Drawing.Size(117, 82);
+            this.noel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.noel.TabIndex = 6;
+            this.noel.TabStop = false;
+            // 
+            // obstaculo6
+            // 
+            this.obstaculo6.BackColor = System.Drawing.Color.Ivory;
+            this.obstaculo6.Location = new System.Drawing.Point(800, 1);
+            this.obstaculo6.Name = "obstaculo6";
+            this.obstaculo6.Size = new System.Drawing.Size(10, 581);
+            this.obstaculo6.TabIndex = 5;
+            this.obstaculo6.TabStop = false;
+            // 
+            // obstaculo5
+            // 
+            this.obstaculo5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.obstaculo5.Location = new System.Drawing.Point(1, 1);
+            this.obstaculo5.Name = "obstaculo5";
+            this.obstaculo5.Size = new System.Drawing.Size(10, 582);
+            this.obstaculo5.TabIndex = 4;
+            this.obstaculo5.TabStop = false;
+            // 
+            // obstaculo4
+            // 
+            this.obstaculo4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.obstaculo4.Location = new System.Drawing.Point(377, 474);
+            this.obstaculo4.Name = "obstaculo4";
+            this.obstaculo4.Size = new System.Drawing.Size(10, 111);
+            this.obstaculo4.TabIndex = 3;
+            this.obstaculo4.TabStop = false;
+            // 
+            // obstaculo3
+            // 
+            this.obstaculo3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.obstaculo3.Location = new System.Drawing.Point(377, 299);
+            this.obstaculo3.Name = "obstaculo3";
+            this.obstaculo3.Size = new System.Drawing.Size(10, 111);
+            this.obstaculo3.TabIndex = 2;
+            this.obstaculo3.TabStop = false;
+            // 
+            // obstaculo2
+            // 
+            this.obstaculo2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.obstaculo2.Location = new System.Drawing.Point(377, 145);
+            this.obstaculo2.Name = "obstaculo2";
+            this.obstaculo2.Size = new System.Drawing.Size(10, 111);
+            this.obstaculo2.TabIndex = 1;
+            this.obstaculo2.TabStop = false;
+            // 
+            // obstaculo
+            // 
+            this.obstaculo.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.obstaculo.Location = new System.Drawing.Point(377, 1);
+            this.obstaculo.Name = "obstaculo";
+            this.obstaculo.Size = new System.Drawing.Size(10, 111);
+            this.obstaculo.TabIndex = 0;
+            this.obstaculo.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(813, 583);
+            this.Controls.Add(this.baston1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.baston2);
+            this.Controls.Add(this.caramelo1);
+            this.Controls.Add(this.caramelo2);
+            this.Controls.Add(this.baston3);
+            this.Controls.Add(this.caramelo3);
+            this.Controls.Add(this.over);
+            this.Controls.Add(this.enemigo2);
+            this.Controls.Add(this.enemigo3);
+            this.Controls.Add(this.enemigo);
+            this.Controls.Add(this.noel);
+            this.Controls.Add(this.obstaculo6);
+            this.Controls.Add(this.obstaculo5);
+            this.Controls.Add(this.obstaculo4);
+            this.Controls.Add(this.obstaculo3);
+            this.Controls.Add(this.obstaculo2);
+            this.Controls.Add(this.obstaculo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.baston1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baston2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caramelo1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caramelo2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baston3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caramelo3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemigo2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemigo3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemigo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.noel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstaculo6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstaculo5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstaculo4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstaculo3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstaculo2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstaculo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox obstaculo;
+        private System.Windows.Forms.PictureBox obstaculo2;
+        private System.Windows.Forms.PictureBox obstaculo3;
+        private System.Windows.Forms.PictureBox obstaculo4;
+        private System.Windows.Forms.PictureBox obstaculo5;
+        private System.Windows.Forms.PictureBox obstaculo6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox noel;
+        private System.Windows.Forms.PictureBox enemigo;
+        private System.Windows.Forms.PictureBox enemigo3;
+        private System.Windows.Forms.PictureBox enemigo2;
+        private System.Windows.Forms.Label over;
+        private System.Windows.Forms.PictureBox caramelo3;
+        private System.Windows.Forms.PictureBox baston3;
+        private System.Windows.Forms.PictureBox caramelo2;
+        private System.Windows.Forms.PictureBox caramelo1;
+        private System.Windows.Forms.PictureBox baston2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox baston1;
+    }
+}
+
---
+++ CandysChristmas/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CandysChristmas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            over.Visible = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Movimiento(acelaracion);
+            Greanch(acelaracion);
+            perder();
+            Puntos(acelaracion);
+            PuntajeMasivo();
+        }
+        int puntaje = 0;
+
+        Random r = new Random();
+        int x, y;
+            
+        void Greanch(int speed)
+        {
+            if (enemigo.Top >= 500)
+            {
+                x=r.Next(0, 200);
+              enemigo.Location = new Point(x, 0);
+
+            }
+            else
+            {
+                enemigo.Top += speed;
+            }
+            if (enemigo2.Top >= 500)
+            {
+                
+                x = r.Next(0, 260);
+                enemigo2.Location = new Point(x, 0);
+
+            }
+            else
+            {
+                enemigo2.Top += speed;
+            }
+
+            if (enemigo3.Top >= 500)
+            {
+            
+                x = r.Next(200, 550);
+                enemigo3.Location = new Point(x, 0);
+
+            }
+            else
+            {
+                enemigo3.Top += speed;
+            }
+        }
+        void Puntos(int speed)
+        {
+            if (caramelo1.Top >= 500)
+            {
+                x = r.Next(0, 200);
+                caramelo1.Location = new Point(x, 0);
+
+            }
+            else
+            {
+                caramelo1.Top += speed;
+            }
+            if (caramelo2.Top >= 500)
+            {
+
+                x = r.Next(0, 260);
+                caramelo2.Location = new Point(x, 0);
+
+            }
+            else
+            {
+                caramelo2.Top += speed;
+            }
+
+            if (caramelo3.Top >= 500)
+            {
+
+                x = r.Next(200, 550);
+                caramelo3.Location = new Point(x, 0);
+
+            }
+            else
+            {
+                caramelo3.Top += speed;
+            }
+            if (baston1.Top >= 500)
+            {
+                x = r.Next(0, 200);
+                baston1.Location = new Point(x, 0);
+
+            }
+            else
+            {
+                baston1.Top += speed;
+            }
+            if (baston2.Top >= 500)
+            {
+
+                x = r.Next(0, 260);
+                baston2.Location = new Point(x, 0);
+
+            }
+            else
+            {
+                baston2.Top += speed;
+            }
+
+            if (baston3.Top >= 500)
+            {
+
+                x = r.Next(200, 550);
+                baston3.Location = new Point(x, 0);
+
+            }
+            else
+            {
+                baston3.Top += speed;
+            }
+
+
+        }
+
+        void Movimiento(int speed)
+        {
+            if(obstaculo.Top>=500 )
+            {
+                obstaculo.Top = 0;
+                
+            }
+            else
+            {
+                obstaculo.Top += speed;
+                
+            }
+            if( obstaculo2.Top >= 500)
+            {
+                obstaculo2.Top = 0;
+            }
+            else
+            {
+                obstaculo2.Top += speed;
+            }
+                 
+                if(obstaculo3.Top >= 500)
+            {
+                obstaculo3.Top = 0;
+            }
+            else
+            {
+                obstaculo3.Top += speed;
+            }
+             if( obstaculo4.Top >= 500)
+            {
+                obstaculo4.Top = 0;
+
+            }
+            else
+            {
+                obstaculo4.Top += speed;
+
+
+            }
+
+        }
+        int acelaracion = 0;
+
+        private void over_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                if (noel.Left > 0)
+                {
+                    noel.Left += -8;
+                }
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                if (noel.Right < 630)
+                {
+                    noel.Left +=8;
+                }
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                if (acelaracion < 21)
+                {
+                    acelaracion++;
+                }
+             }
+            if (e.KeyCode == Keys.Down)
+            {
+                if (acelaracion > 0)
+                {
+                    acelaracion--;
+                }
+            }
+
+        }
+        void perder()
+        {
+            if (noel.Bounds.IntersectsWith(enemigo.Bounds))
+            {
+                timer1.Enabled = false;
+                over.Visible = true;
+            }
+            if (noel.Bounds.IntersectsWith(enemigo2.Bounds))
+            {
+                timer1.Enabled = false;
+                over.Visible = true;
+            }
+            if (noel.Bounds.IntersectsWith(enemigo3.Bounds))
+            {
+                timer1.Enabled = false;
+                over.Visible = true;
+            }
+        }
+        void PuntajeMasivo()
+        {
+            if (noel.Bounds.IntersectsWith(caramelo1.Bounds))
+            {
+                puntaje++;
+                label1.Text = "Caramelos" + puntaje.ToString();
+                x = r.Next(0, 200);
+                caramelo1.Location = new Point(x, 0);
+
+            }
+            if (noel.Bounds.IntersectsWith(caramelo2.Bounds))
+            {
+                puntaje++;
+                label1.Text = "Caramelos" + puntaje.ToString();
+                x = r.Next(0, 200);
+                caramelo2.Location = new Point(x, 0);
+            }
+            if (noel.Bounds.IntersectsWith(caramelo3.Bounds))
+            {
+                puntaje++;
+                label1.Text = "Caramelos" + puntaje.ToString();
+                x = r.Next(0, 200);
+                caramelo3.Location = new Point(x, 0);
+            }
+            if (noel.Bounds.IntersectsWith(baston1.Bounds))
+            {
+                puntaje++;
+                label1.Text = "Caramelos" + puntaje.ToString();
+                x = r.Next(0, 200);
+                baston1.Location = new Point(x, 0);
+            }
+            if (noel.Bounds.IntersectsWith(baston2.Bounds))
+            {
+                puntaje++;
+                label1.Text = "Caramelos" + puntaje.ToString();
+                x = r.Next(0, 200);
+                baston2.Location = new Point(x, 0);
+            }
+            if (noel.Bounds.IntersectsWith(baston3.Bounds))
+            {
+                puntaje++;
+                label1.Text = "Caramelos" + puntaje.ToString();
+                x = r.Next(0, 200);
+                baston3.Location = new Point(x, 0);
+            }
+        }
+    }
+}
